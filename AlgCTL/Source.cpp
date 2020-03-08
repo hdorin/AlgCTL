efa048eb6d2b9248196815fb26386d1579b7a364
hdorin <hdorin@users.noreply.github.com>
2020-03-08 18:01:38 +0200

    Am facut citirea

---
+++ AlgCTL/Source.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<fstream>
+#include<cstring>
+using namespace std;
+struct kripkeStructure {
+	int worldsCount;
+	char worldsNames[100][10];
+	int worldsRelations[100][100];
+	char wordlsLabels[100][20];
+
+};
+void readKripreStructure(kripkeStructure &ks) {
+	char aux[100], *auxp;
+	int i, j;
+	ifstream f("kripke_structure.txt");
+	f.getline(aux, 100);//skipping files comments
+	
+	f >> ks.worldsCount;
+	
+	f.get();
+	f.getline(aux, 100);//skipping files comments
+	
+	for (i = 0; i < ks.worldsCount; i++) {
+		f >> ks.worldsNames[i];
+	}
+	
+	f.get();
+	f.getline(aux, 100);//skipping files comments
+	
+	for (i = 0; i < ks.worldsCount; i++) {
+		j = -1;
+		do {
+			j++;
+			f >> ks.wordlsLabels[i][j];
+		} while (ks.wordlsLabels[i][j] != '-');
+		ks.wordlsLabels[i][j] = '\0';
+	}
+
+	f.get();
+	f.getline(aux, 100);//skipping files comments
+	
+	char worldName[10];
+	for (i = 0; i < ks.worldsCount; i++) {
+		for (j = 0; j < ks.worldsCount; j++) {
+			ks.worldsRelations[i][j] = 0;
+		}
+	}
+	for (i = 0; i < ks.worldsCount; i++) {
+		j = -1;
+		while(true) {
+			f >> worldName;
+			if (strcmp(worldName, "-") == 0) {
+				break;
+			}
+			for (j = 0; j < ks.worldsCount; j++) {
+				if (strcmp(worldName, ks.worldsNames[j]) == 0) {
+					ks.worldsRelations[i][j] = 1;
+				}
+			}
+		}
+	}
+	f.close();
+
+}
+void printKripkeStructure(kripkeStructure ks) {
+	int i,j;
+	cout << ks.worldsCount << endl;
+	for (i = 0; i < ks.worldsCount; i++) {
+		cout << ks.worldsNames[i] << " ";
+	}
+	cout << endl;
+	for (i = 0; i < ks.worldsCount; i++) {
+		if (strlen(ks.wordlsLabels[i]) == 0) {
+			cout << "-" << endl;
+		}
+		else {
+			cout << ks.wordlsLabels[i] << endl;
+		}
+
+	}
+	for (i = 0; i < ks.worldsCount; i++) {
+		for (j = 0; j < ks.worldsCount; j++) {
+			cout << ks.worldsRelations[i][j]<<" ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+int main() {
+	kripkeStructure ks;
+	readKripreStructure(ks);
+	printKripkeStructure(ks);
+	int x;
+	cin >> x;
+}
